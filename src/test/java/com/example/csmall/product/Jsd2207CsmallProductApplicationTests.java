61d19000fcc5e33c63d6976832b95504cb77f3c9
Administrator <2359304261@qq.com>
2022-10-24 11:42:14 +0800

    c1

---
+++ src/test/java/com/example/csmall/product/Jsd2207CsmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsd2207CsmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
