f0e042b47b78a0afd35a2714352163422d717390
Administrator <2359304261@qq.com>
2022-11-07 09:05:28 +0800

    v6

--- src/main/java/cn/tedu/csmall/product/controller/AttributeController.java
+++ src/main/java/cn/tedu/csmall/product/controller/AttributeController.java
@@ -9,7 +9,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
